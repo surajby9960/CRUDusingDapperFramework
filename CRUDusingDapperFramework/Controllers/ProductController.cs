1ee22ca4cc87d829938efb45a267e2769e007b78
surajby9960 <surajbyadav25@gmail.com>
2022-07-27 17:37:31 +0530

    Add project files.

---
+++ CRUDusingDapperFramework/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDusingDapperFramework.Models;
+
+namespace CRUDusingDapperFramework.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var model = ProductDAL.GetAllProducts();
+            return View(model);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                int result = ProductDAL.AddProduct(prod);
+                if (result == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product prod)
+        {
+            try
+            {
+                int result = ProductDAL.UpdateProduct(prod);
+                if (result == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                int result = ProductDAL.DeleteProduct(id);
+                if (result == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUDusingDapperFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDusingDapperFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
